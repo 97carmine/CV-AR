e9fca713d5d53dc2cd830d8e043a1ff8cf4da3f1
Axel Gabriel Calle Granda <97carmine@protonmail.com>
2020-04-21 21:24:46 +0200

    Renombradas funciones usando CamelCase

--- src/js/detect_languaje.js
+++ src/js/detect_languaje.js
@@ -1,10 +1,11 @@
-function detect_languaje() {
+function detectLanguaje() {
     var idioma = navigator.language;
     return idioma;
 }
 
+// eslint-disable-next-line no-unused-vars
 function redirect( idioma ) {
-    if ( detect_languaje() === "es-ES" ) {
+    if ( detectLanguaje() === "es-ES" ) {
         location.href = "es/index.html";
     } else {
         location.href = "en/index.html";
