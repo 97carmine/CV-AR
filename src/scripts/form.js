141324dae094a24d87d05ffa13cfbc942d75d4ad
Axel Gabriel Calle Granda <97carmine@protonmail.com>
2020-04-23 16:05:33 +0200

    Movidos o creados y actualizados

---
+++ src/scripts/form.js
@@ -0,0 +1,3 @@
+import { getLocation } from "../components/set_geolocalization.js";
+
+window.getLocation = getLocation;
---
+++ src/scripts/templates.js
@@ -0,0 +1,3 @@
+$( "#link_example_1" ).attr( "href", "../examples/example_1.html" );
+$( "#link_example_2" ).attr( "href", "../examples/example_2.html" );
+$( "#link_example_3" ).attr( "href", "../examples/example_3.html" );
