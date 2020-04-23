73a60262d62cfc5175426f749b7ef8a0557fb62b
Axel Gabriel Calle Granda <97carmine@protonmail.com>
2020-04-23 16:06:34 +0200

    Eliminados viejos ficheros JavaScript

--- src/js/templates.js
+++
@@ -1,3 +0,0 @@
-$( "#link_example_1" ).attr( "href", "../examples/example_1.html" );
-$( "#link_example_2" ).attr( "href", "../examples/example_2.html" );
-$( "#link_example_3" ).attr( "href", "../examples/example_3.html" );
