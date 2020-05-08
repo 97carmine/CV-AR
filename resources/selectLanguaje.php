e71bb5cda66ca447188b6174babce6378cd11b57
97carmine <97carmine@protonmail.com>
2020-05-08 13:00:32 +0200

    Modificado función para la detección de gettext

--- resources/selectLanguaje.php
+++ resources/selectLanguaje.php
@@ -1,7 +1,16 @@
 <?php
-    $locale = substr($_SERVER["HTTP_ACCEPT_LANGUAGE"],0,5);
-    putenv("LC_ALL=$locale");
-    setlocale(LC_ALL, $locale);
-    bindtextdomain("default", "../locale");
-    textdomain("default");
+    $locale = substr($_SERVER["HTTP_ACCEPT_LANGUAGE"],0,2);
+    $supported_locales = [
+        "es",
+        "fr"
+    ];
+    
+    if(array_search($locale, $supported_locales) != false) {
+        $domain = "default";
+
+        putenv("LC_ALL=" . $locale);
+        setlocale(LC_ALL, $locale);
+        bindtextdomain($domain, "../locale");
+        textdomain($domain);
+    }
 ?>
